74467c13ac69933ad391a8569e3574742df6139e
Jaylen N <jaylen.nav@gmail.com>
2019-08-28 21:33:16 +1000

    add express

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const ptv = require('ptv-api')
 const config = require('./config.json')
+const express = require('express')
+const app = express()
 
 ptvClient = ptv(config.userid, config.apikey)
 ptvClient.then(apis => {
@@ -15,3 +17,11 @@ ptvClient.then(apis => {
         console.log(`Service: ${route.route_name}, Status: ${route_service_status.description}`)
       })
 }).catch(console.error)
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+const server = app.listen(3000, () => {
+    console.log(`Express running → PORT ${server.address().port}`)
+})
