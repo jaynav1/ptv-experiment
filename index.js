8c99a65466fc6de42ade5ca33112546cd65a5141
Jaylen N <jaylen.nav@gmail.com>
2019-08-28 22:07:17 +1000

    yes

--- index.js
+++ index.js
@@ -3,23 +3,25 @@ const config = require('./config.json')
 const express = require('express')
 const app = express()
 
-ptvClient = ptv(config.userid, config.apikey)
-ptvClient.then(apis => {
-    return apis.Routes.Routes_OneOrMoreRoutes({ route_types: '0' })
-}).then(res => {
-    data = res.body
-    status = data.status
-    routes = data.routes
-    console.log(`Health is (1 = good) : ${status.health}`)
-    console.log(routes)
-    routes.forEach(function(route) {
-        route_service_status = route.route_service_status
-        console.log(`Service: ${route.route_name}, Status: ${route_service_status.description}`)
-      })
-}).catch(console.error)
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    ptvClient = ptv(config.userid, config.apikey)
+    ptvClient.then(apis => {
+        return apis.Routes.Routes_OneOrMoreRoutes({ route_types: '0' })
+    }).then(resp => {
+        data = resp.body
+        status = data.status
+        routes = data.routes
+        sitebody = []
+        console.log(`Health is (1 = good) : ${status.health}`)
+        console.log(routes)
+        routes.forEach(function(route) {
+            route_service_status = route.route_service_status
+            sitebody.push([`Service: ${route.route_name}, Status: ${route_service_status.description}`])
+        })
+        res.send(sitebody)
+    }).catch(console.error)
 })
 
 const server = app.listen(3000, () => {
