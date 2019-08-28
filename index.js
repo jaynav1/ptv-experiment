6d434344366063e6cec206a9cdeb4e395aaf1a02
Jaylen N <jaylen.nav@gmail.com>
2019-08-28 21:26:21 +1000

    factor data

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const ptv = require('ptv-api')
+const config = require('./config.json')
 
-
+ptvClient = ptv(config.userid, config.apikey)
+ptvClient.then(apis => {
+    return apis.Routes.Routes_OneOrMoreRoutes({ route_types: '0' })
+}).then(res => {
+    data = res.body
+    status = data.status
+    routes = data.routes
+    console.log(`Health is (1 = good) : ${status.health}`)
+    console.log(routes)
+    routes.forEach(function(route) {
+        route_service_status = route.route_service_status
+        console.log(`Service: ${route.route_name}, Status: ${route_service_status.description}`)
+      })
+}).catch(console.error)
